5c7924a3c1c83615e290567d6576211a977ff821
Vincent Damen <vinster1997@gmail.com>
2017-11-02 10:15:22 +0100

    Fixed the application, i'm done

--- MrPotatoHead/app/src/main/java/com/example/vincent/myapplication/MrPotatoHead.java
+++ MrPotatoHead/app/src/main/java/com/example/vincent/myapplication/MrPotatoHead.java
@@ -1,5 +1,6 @@
 package com.example.vincent.myapplication;
 
+import android.support.v4.content.res.ConfigurationHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,16 +9,12 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 
 public class MrPotatoHead extends AppCompatActivity {
-    CheckBox Arms;
-    CheckBox Eyes;
-    CheckBox Eyebrows;
-    CheckBox Ears;
-    CheckBox Nose;
-    CheckBox Shoes;
-    CheckBox Mustache;
-    CheckBox Mouth;
-    CheckBox Glasses;
-    CheckBox Hat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mr_potato_head);
+    }
+
     public void onCheckboxClicked(View view) {
         boolean check = ((CheckBox) view).isChecked();
         switch (view.getId()) {
@@ -136,76 +133,92 @@ public class MrPotatoHead extends AppCompatActivity {
     }
     public void onSaveInstanceState(Bundle Outstate){
         super.onSaveInstanceState(Outstate);
-        Outstate.putBoolean("Arms",Arms.isChecked());
-        Outstate.putBoolean("Ears",Ears.isChecked());
-        Outstate.putBoolean("Eyebrows",Eyebrows.isChecked());
-        Outstate.putBoolean("Eyes",Eyes.isChecked());
-        Outstate.putBoolean("Glasses",Glasses.isChecked());
-        Outstate.putBoolean("Hat",Hat.isChecked());
-        Outstate.putBoolean("Mouth",Mouth.isChecked());
-        Outstate.putBoolean("Mustache",Mustache.isChecked());
-        Outstate.putBoolean("Nose",Nose.isChecked());
-        Outstate.putBoolean("Shoes",Shoes.isChecked());
+        Outstate.putInt("Arms",findViewById(R.id.imageArms).getVisibility());
+        Outstate.putInt("Ears",findViewById(R.id.imageEars).getVisibility());
+        Outstate.putInt("Eyebrows",findViewById(R.id.imageEyebrows).getVisibility());
+        Outstate.putInt("Eyes",findViewById(R.id.imageEyes).getVisibility());
+        Outstate.putInt("Glasses",findViewById(R.id.imageGlasses).getVisibility());
+        Outstate.putInt("Hat",findViewById(R.id.imageHat).getVisibility());
+        Outstate.putInt("Mouth",findViewById(R.id.imageMouth).getVisibility());
+        Outstate.putInt("Mustache",findViewById(R.id.imageMustache).getVisibility());
+        Outstate.putInt("Nose",findViewById(R.id.imageNose).getVisibility());
+        Outstate.putInt("Shoes",findViewById(R.id.imageShoes).getVisibility());
 
     }
     public void onRestoreInstanceState(Bundle Instate){
-        Boolean ArmsR = Instate.getBoolean("Arms");
-        Boolean EarsR = Instate.getBoolean("Ears");
-        Boolean EyesR = Instate.getBoolean("Eyes");
-        Boolean EyebrowsR = Instate.getBoolean("Eyebrows");
-        Boolean GlassesR = Instate.getBoolean("Glasses");
-        Boolean HatR = Instate.getBoolean("Hat");
-        Boolean MouthR = Instate.getBoolean("Mouth");
-        Boolean MustacheR = Instate.getBoolean("Mustache");
-        Boolean NoseR = Instate.getBoolean("Nose");
-        Boolean ShoesR = Instate.getBoolean("Shoes");
-        if(ArmsR){
+        Integer ArmsR = Instate.getInt("Arms");
+        Integer EarsR = Instate.getInt("Ears");
+        Integer EyesR = Instate.getInt("Eyes");
+        Integer EyebrowsR = Instate.getInt("Eyebrows");
+        Integer GlassesR = Instate.getInt("Glasses");
+        Integer HatR = Instate.getInt("Hat");
+        Integer MouthR = Instate.getInt("Mouth");
+        Integer MustacheR = Instate.getInt("Mustache");
+        Integer NoseR = Instate.getInt("Nose");
+        Integer ShoesR = Instate.getInt("Shoes");
+        if(ArmsR==0){
             ImageView image = (ImageView) findViewById(R.id.imageArms);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Arms);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(EarsR){
+        if(EarsR==0){
             ImageView image = (ImageView) findViewById(R.id.imageEars);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Ears);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
 
         }
-        if(EyesR){
+        if(EyesR==0){
             ImageView image = (ImageView) findViewById(R.id.imageEyes);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Eyes);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(EyebrowsR){
+        if(EyebrowsR==0){
             ImageView image = (ImageView) findViewById(R.id.imageEyebrows);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Eyebrows);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(GlassesR){
+        if(GlassesR==0){
             ImageView image = (ImageView) findViewById(R.id.imageGlasses);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Glasses);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(HatR){
+        if(HatR==0){
             ImageView image = (ImageView) findViewById(R.id.imageHat);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Hat);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(MouthR){
+        if(MouthR==0){
             ImageView image = (ImageView) findViewById(R.id.imageMouth);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Mouth);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(MustacheR){
+        if(MustacheR==0){
             ImageView image = (ImageView) findViewById(R.id.imageMustache);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Mustache);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(NoseR){
+        if(NoseR==0){
             ImageView image = (ImageView) findViewById(R.id.imageNose);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Nose);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
-        if(ShoesR){
+        if(ShoesR==0){
             ImageView image = (ImageView) findViewById(R.id.imageShoes);
+            CheckBox checkBox = (CheckBox) findViewById(R.id.Shoes);
+            checkBox.setChecked(!checkBox.isChecked());
             image.setVisibility(View.VISIBLE);
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mr_potato_head);
-    }
+
 
 }
