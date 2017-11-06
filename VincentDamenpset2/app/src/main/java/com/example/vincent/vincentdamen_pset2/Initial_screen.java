87dc9bea0bd7cc9ea4ce1bfe3decb5cca099b9a0
Vincent Damen <vinster1997@gmail.com>
2017-11-06 12:29:59 +0100

    Added ass 2 and made the first activity

---
+++ VincentDamenpset2/app/src/main/java/com/example/vincent/vincentdamen_pset2/Initial_screen.java
@@ -0,0 +1,14 @@
+package com.example.vincent.vincentdamen_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Initial_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_screen);
+    }
+    
+}
