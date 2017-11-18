4d16f4a19bd6e8b193444a2a0bcbfeb5b8bc57ec
Vincent Damen <vinster1997@gmail.com>
2017-11-18 18:36:54 +0100

    Almost finished the app, only need to fix updating on press/long press

--- ToDolist/app/src/main/java/com/example/vincent/to_dolist/MainActivity.java
+++ ToDolist/app/src/main/java/com/example/vincent/to_dolist/MainActivity.java
@@ -1,18 +1,71 @@
 package com.example.vincent.to_dolist;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+    private TodoAdapter adapter;
+    private ToDoDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = ToDoDatabase.getInstance(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToDoDatabase db = ToDoDatabase.getInstance(getApplicationContext());
-    }
+        Cursor overview = db.selectAll();
+        adapter = new TodoAdapter( this,overview);
+        ListView list = (ListView) findViewById(R.id.List);
+        list.setOnItemClickListener(new shortPress());
+        //list.setOnItemLongClickListener(new longPress());
+        list.setAdapter(adapter);
 
+        }
+
+    private class shortPress implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Context context = getApplicationContext();
+            CharSequence text = "Thisworks";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            CheckBox complete = (CheckBox)view.findViewById(R.id.complete);
+            Cursor overview = db.selectAll();
+            overview.move(i);
+            int id= overview.getInt(overview.getColumnIndex("_id"));
+            if(complete.isChecked()){
+                db.update(id, 0);
+
+            }
+            else{
+                db.update(id,1);
+            }
+            updateData();
+        }
+
+    }
+    //private class longPress implements AdapterView.OnItemLongClickListener{
+    //   @Override
+    //    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+    //        return false;
+    //    }
+
+    //}
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -28,4 +81,24 @@ public class MainActivity extends AppCompatActivity {
         String text = inState.getString("editor");
         editor.setText(text);
     }
+
+    public void addItem(View view){
+        EditText editor = (EditText) findViewById(R.id.Editor);
+        String ToDo = editor.getText().toString();
+        editor.setText("");
+        if(ToDo.length()>0){
+        ToDoDatabase db = ToDoDatabase.getInstance(getApplicationContext());
+        db.insert(ToDo,0);
+        updateData();}
+    }
+
+    private void updateData(){
+        db = ToDoDatabase.getInstance(getApplicationContext());
+        Cursor overview1 = db.selectAll();
+        adapter.swapCursor(overview1);
+        ListView list = (ListView) findViewById(R.id.List);
+        list.setAdapter(adapter);
+    }
+
+
 }
--- ToDolist/app/src/main/java/com/example/vincent/to_dolist/ToDoDatabase.java
+++ ToDolist/app/src/main/java/com/example/vincent/to_dolist/ToDoDatabase.java
@@ -1,9 +1,13 @@
 package com.example.vincent.to_dolist;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class ToDoDatabase extends SQLiteOpenHelper {
@@ -20,24 +24,50 @@ public class ToDoDatabase extends SQLiteOpenHelper {
         return instance;
     }
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table todos (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table todos (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                                "title TEXT, completed INTEGER);");
+        addToDoTest();
+    }
+    public void addToDoTest(){
+        SQLiteDatabase db=getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("title","Do the dishes");
         values.put("completed",0);
+        db.insert("todos",null,values);
         values.put("title","Do the laundry");
-        values.put("completed",0);
+        values.put("completed",1);
+        db.insert("todos",null,values);
         values.put("title","Get food for tonight");
         values.put("completed",0);
-        SQLiteDatabase db =this.getWritableDatabase();
         db.insert("todos",null,values);
 
     }
+    public void insert(String title, int completed) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("title", title);
+        values.put("completed", completed);
+        db.insert("todos",null,values);
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + "todos");
-        onCreate(sqLiteDatabase);
     }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + "todos");
+        onCreate(db);
+    }
+    public Cursor selectAll(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor =db.rawQuery("SELECT * FROM todos",null);
+
+        return cursor;
+    }
+    public void update(long id, int completed){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("completed", completed);
+        db.update("todos",values,"_id="+id, null);
+
+    }
+
 }
