68688ef8b392b5926093739d7f319271cd25fa74
Vincent Damen <vinster1997@gmail.com>
2017-12-11 17:37:01 +0100

    Fixed the timer for the game

--- PokeBattler/app/src/main/java/com/example/vincent/pokebattler/BattleScreen.java
+++ PokeBattler/app/src/main/java/com/example/vincent/pokebattler/BattleScreen.java
@@ -1,13 +1,21 @@
 package com.example.vincent.pokebattler;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.AdapterView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,23 +24,110 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BattleScreen extends Fragment {
     private Firebase firebase;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final ArrayList<battles> chosenBattles = new ArrayList<battles>();
+    public String numberA = "34";
+    public String numberB = "3";
+    private CountDownTimer timer;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState){
         // Inflate the layout for this fragment
-        //database.setPersistenceEnabled(true);
-        return inflater.inflate(R.layout.fragment_battle_screen, container, false);
+        View view= inflater.inflate(R.layout.fragment_battle_screen, container, false);
+        TextView start = view.findViewById(R.id.Start);
+        start.setOnClickListener(new startGame());
+        FloatingActionButton A = view.findViewById(R.id.infoA);
+        FloatingActionButton B = view.findViewById(R.id.InfoB);
+        B.setOnClickListener(new ShowDetails());
+        A.setOnClickListener(new ShowDetails());
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference nDatabase = database.getReference("Battles");
+
+        nDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long Amountentries = dataSnapshot.getChildrenCount();
+                int total = (int)Amountentries;
+                while (chosenBattles.size()<2000){
+                    battles chosenBattle = dataSnapshot.child(getRandomInt(total))
+                            .getValue(battles.class);
+                    chosenBattles.add(chosenBattle);
+                }}
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }});
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+    public static String getRandomInt(int total) {
+        int chosen = new Random().nextInt(total+1);
+        return chosen+"";}
+
+    private class startGame implements View.OnClickListener{
+        @Override
+        public void onClick(final View view) {
+            Timerfunction(0);
+
+        }}
+
+
+    private class ShowDetails implements View.OnClickListener {
+        //makes a onClick event to add a meal to your order
+        @Override
+        public void onClick(View view) {
+            ProgressBar bar = getView().findViewById(R.id.Timer);
+            switch (view.getId()){
+                case(R.id.infoA):
+                    openDialog(numberA);
+                    break;
+                case (R.id.InfoB):
+                    openDialog(numberB);
+                    break;}}}
+
+    public void Timerfunction(int time){
+        if (time==0);{
+            time=60000;
+        }
+
+        final TextView start = getView().findViewById(R.id.Start);
+        final ProgressBar bar = getView().findViewById(R.id.Timer);
+        start.setClickable(false);
+        bar.setMax(60000);
+        start.setText("Who's gonna win this battle?");
+
+        timer = new CountDownTimer(time, 100) {
+
+            public void onTick(long millisUntilFinished) {
+                bar.setProgress(60000-(int)millisUntilFinished);
+            }
+
+            public void onFinish() {
+                start.setText("Wanna play again? Click me");
+                start.setClickable(true);
+
+            }
+        }.start();
+    }
+    public void openDialog(String no) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        PokemonInfo fragment3 = new PokemonInfo().newInstance(no);
+        fragment3.show(ft, "dialog");
     }
     @Override
     public void onStart() {
--- PokeBattler/app/src/main/java/com/example/vincent/pokebattler/MainActivity.java
+++ PokeBattler/app/src/main/java/com/example/vincent/pokebattler/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.vincent.pokebattler;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,18 +35,12 @@ public class MainActivity extends AppCompatActivity {
                     ft.commit();
                     return true;
                 case R.id.navigation_highscores:
-                    FirebaseAuth.getInstance().signOut();
-                    onStart();
-                    return true;
-                case R.id.navigation_settings:
-                    FragmentManager fm1 = getSupportFragmentManager();
-                    BattleScreen fragment1 = new BattleScreen();
-                    FragmentTransaction ft1 = fm1.beginTransaction();
-                    ft1.replace(R.id.Fragment_container, fragment1);
-                    ft1.commit();
-                    return true;
-                case R.id.navigation_pokedex:
 
+                    return true;
+                case R.id.navigation_signout:
+                    SignOut();
+                    return false;
+                case R.id.navigation_pokedex:
                     FragmentManager fm2 = getSupportFragmentManager();
                     PokeDex fragment2 = new PokeDex();
                     FragmentTransaction ft2 = fm2.beginTransaction();
@@ -55,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
                     ft2.commit();
                     return true;
                 case R.id.navigation_train:
+                    FragmentManager fm1 = getSupportFragmentManager();
+                    BattleScreen fragment1 = new BattleScreen();
+                    FragmentTransaction ft1 = fm1.beginTransaction();
+                    ft1.replace(R.id.Fragment_container, fragment1);
+                    ft1.commit();
                     return true;
             }
             return false;
@@ -87,11 +85,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         this.finish();
+    }
+
+    public void SignOut(){
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setMessage("Do you really want to leave me?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Stay",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Leave",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        FirebaseAuth.getInstance().signOut();
+                        onStart();
+                    }
+                });
+        alertDialog.show();
     }
 }
